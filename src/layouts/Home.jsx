9bae60cac70fda2baca67a1da4eb59c643a6d392
jaky-sheikh <jaky@eu4m.eu>
2025-01-18 22:30:17 +0600

    first commit

---
+++ src/layouts/Home.jsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+
+const Home = () => {
+    return (
+        <div>
+            <header>
+                <Header></Header>
+            </header>
+            <nav></nav>
+            <main></main>
+        </div>
+    );
+};
+
+export default Home;
