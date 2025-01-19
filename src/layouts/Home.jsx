700e0c010e560dcc7b7510bee95967cbce811dd5
jaky-sheikh <jaky@eu4m.eu>
2025-01-19 19:07:14 +0600

    Newscard created

--- src/layouts/Home.jsx
+++ src/layouts/Home.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import LatestNews from "../components/LatestNews";
 import LeftNavbar from "../components/layout-components/LeftNavbar";
@@ -18,7 +19,9 @@ const Home = () => {
             </nav>
             <main className="w-11/12 mx-auto pt-5 grid grid-cols-12 gap-2">
                 <aside className="col-span-3"><LeftNavbar></LeftNavbar></aside>
-                <section className="col-span-6">Main</section>
+                <section className="col-span-6">
+                    <Outlet></Outlet>
+                </section>
                 <aside className="col-span-3">
                     <RightNavbar></RightNavbar>
                 </aside>
