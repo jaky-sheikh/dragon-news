157c085eba7cd44d0cb3e89b18db25782346ed3b
jaky-sheikh <jaky@eu4m.eu>
2025-01-19 12:36:29 +0600

    leftside section done

--- src/layouts/Home.jsx
+++ src/layouts/Home.jsx
@@ -1,17 +1,25 @@
 import Header from "../components/Header";
 import LatestNews from "../components/LatestNews";
+import LeftNavbar from "../components/layout-components/LeftNavbar";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
     return (
-        <div>
+        <div className="font-poppins">
             <header>
                 <Header></Header>
                 <section className="w-11/12 mx-auto">
                     <LatestNews></LatestNews>
                 </section>
             </header>
-            <nav></nav>
-            <main></main>
+            <nav className="w-11/12 mx-auto py-2">
+                <Navbar></Navbar>
+            </nav>
+            <main className="w-11/12 mx-auto pt-5 grid grid-cols-12 gap-2">
+                <aside className="col-span-3"><LeftNavbar></LeftNavbar></aside>
+                <section className="col-span-6">Main</section>
+                <aside className="col-span-3">Right side</aside>
+            </main>
         </div>
     );
 };
