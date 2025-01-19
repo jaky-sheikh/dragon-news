895e4f2de9e8456d4e549c569d6074cb595868d0
jaky-sheikh <jaky@eu4m.eu>
2025-01-19 22:04:32 +0600

    login and register page setup with react router

---
+++ src/pages/Login.jsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+
+const Login = () => {
+    return (
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content flex-col">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Login now!</h1>
+
+                </div>
+                <div className="card bg-base-100 w-full max-w-lg shrink-0 p-10">
+                    <form className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Login</button>
+                        </div>
+                    </form>
+                    <p className="text-semibold text-center">Don't have an account? Please <Link to="/auth/register"><strong>Register</strong></Link>.</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/pages/Register.jsx
@@ -0,0 +1,49 @@
+import { Link } from "react-router-dom";
+
+const Register = () => {
+    return (
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content flex-col">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Register</h1>
+
+                </div>
+                <div className="card bg-base-100 w-full max-w-lg shrink-0 p-10">
+                    <form className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" name="name" placeholder="Name" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Photo-url</span>
+                            </label>
+                            <input type="text" name="photo" placeholder="Photo-url" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Register</button>
+                        </div>
+                    </form>
+                    <p className="text-semibold text-center">Already have an account? Please <Link to="/auth/login"><strong>Login</strong></Link>.</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
