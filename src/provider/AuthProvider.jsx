28e8942df3b5fd92067dfe920872d8b0cab4bd46
jaky-sheikh <jaky@eu4m.eu>
2025-01-21 22:30:56 +0600

    private router function

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -10,17 +10,20 @@ const auth = getAuth(app);
 const AuthProvider = ({ children }) => {
 
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     const createNewUser = (email, password) => {
+        setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password);
     }
 
     const userLogin = (email, password) => {
-        // setLoading(true);
+        setLoading(true);
         return signInWithEmailAndPassword(auth, email, password);
     };
 
     const logOut = () => {
+        setLoading(true);
         return signOut(auth);
     }
 
@@ -29,12 +32,14 @@ const AuthProvider = ({ children }) => {
         setUser,
         createNewUser,
         logOut,
-        userLogin
+        userLogin,
+        loading
     }
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
+            setLoading(false);
         })
         return () => {
             unsubscribe();
